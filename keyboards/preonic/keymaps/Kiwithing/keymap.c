794064e54bfab81bfcfc2eee9348717b392ca6d9
Shara Miller <smiller@cliquestudios.com>
2021-01-19 09:41:45 -0600

    Formatting changes.

--- keyboards/preonic/keymaps/Kiwithing/keymap.c
+++ keyboards/preonic/keymaps/Kiwithing/keymap.c
@@ -17,141 +17,99 @@
 #include QMK_KEYBOARD_H
 #include "muse.h"
 
-enum preonic_layers {
-  _QWERTY,
-  _FUNCT,
-  _LOWER,
-  _RAISE,
-  _ADJUST
-};
+enum preonic_layers { _QWERTY, _FUNCT, _LOWER, _RAISE, _ADJUST };
 
-enum preonic_keycodes {
-  QWERTY = SAFE_RANGE,
-  FUNCT,
-  LOWER,
-  RAISE,
-  ADJUST
-};
+enum preonic_keycodes { QWERTY = SAFE_RANGE, FUNCT, LOWER, RAISE, ADJUST };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-        //Base layer
-        [_QWERTY] = LAYOUT_preonic_1x2uC(
-            KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
-            KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL,
-            KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
-            KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT,
-            FUNCT, KC_LCTL, KC_LGUI, KC_LALT, LOWER, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
-            ),
+    // Base layer
+    [_QWERTY] = LAYOUT_preonic_1x2uC(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL, KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT, FUNCT, KC_LCTL, KC_LGUI, KC_LALT, LOWER, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT),
 
-        //Function layer
-        [_FUNCT] = LAYOUT_preonic_1x2uC(
-            KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
-            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, _______,
-            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-            _______, _______, _______, _______, LOWER, _______, RAISE, _______, _______, _______, _______
-            ),
-        //Symbol layer
-        [_LOWER] = LAYOUT_preonic_1x2uC(
-            KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-            _______, KC_NO, KC_NO, KC_NO, KC_UNDS, KC_PIPE, KC_PLUS, KC_LCBR, KC_RCBR, _______, _______, _______,
-            _______, KC_NO, KC_NO, KC_NO, KC_MINS, KC_BSLS, KC_EQL, KC_LBRC, KC_RBRC, _______, _______, _______,
-            _______, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, _______, _______, _______, _______,
-            _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
-            ),
+    // Function layer
+    [_FUNCT] = LAYOUT_preonic_1x2uC(KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, LOWER, _______, RAISE, _______, _______, _______, _______),
 
-        //Media layer
-        [_RAISE] = LAYOUT_preonic_1x2uC(
-            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-            KC_SLEP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-            _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BRIU, KC_BRID, KC_MSTP,
-            _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
-            ),
+    // Symbol layer
+    [_LOWER] = LAYOUT_preonic_1x2uC(KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, _______, KC_NO, KC_NO, KC_NO, KC_UNDS, KC_PIPE, KC_PLUS, KC_LCBR, KC_RCBR, _______, _______, _______, _______, KC_NO, KC_NO, KC_NO, KC_MINS, KC_BSLS, KC_EQL, KC_LBRC, KC_RBRC, _______, _______, _______, _______, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END),
 
-        //Adjust layer
-        [_ADJUST] = LAYOUT_preonic_1x2uC(
-            RESET, DEBUG, _______, _______, _______, _______, _______, _______, _______, _______, TERM_ON, TERM_OFF,
-            _______, _______, _______, MU_MOD, AU_ON, AU_OFF, CK_TOGG, CK_UP, CK_DOWN, _______, _______, _______,
-            EEP_RST, _______, _______, MUV_DE, MUV_IN, MU_ON, MU_OFF, MI_ON, MI_OFF, _______, _______, _______,
-            _______, RGB_MOD, RGB_HUI, RGB_HUD, _______, _______, _______, _______, RGB_VAI, RGB_VAD, RGB_TOG, _______,
-            _______, RGB_RMOD, RGB_SAI, RGB_SAD, _______, _______, _______, RGB_SPI, RGB_SPD, _______, _______
-            ),
- };
+    // Media layer
+    [_RAISE] = LAYOUT_preonic_1x2uC(_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_SLEP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BRIU, KC_BRID, KC_MSTP, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY),
 
+    // Adjust layer
+    [_ADJUST] = LAYOUT_preonic_1x2uC(RESET, DEBUG, _______, _______, _______, _______, _______, _______, _______, _______, TERM_ON, TERM_OFF, _______, _______, _______, MU_MOD, AU_ON, AU_OFF, CK_TOGG, CK_UP, CK_DOWN, _______, _______, _______, EEP_RST, _______, _______, MUV_DE, MUV_IN, MU_ON, MU_OFF, MI_ON, MI_OFF, _______, _______, _______, _______, RGB_MOD, RGB_HUI, RGB_HUD, _______, _______, _______, _______, RGB_VAI, RGB_VAD, RGB_TOG, _______, _______, RGB_RMOD, RGB_SAI, RGB_SAD, _______, _______, _______, RGB_SPI, RGB_SPD, _______, _______),
+};
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
+    switch (keycode) {
         case QWERTY:
-          if (record->event.pressed) {
-            set_single_persistent_default_layer(_QWERTY);
-          }
-          return false;
-          break;
-        case FUNCT: //Probably not needed?
-          if (record->event.pressed) {
-            layer_on(_FUNCT);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          } else {
-            layer_off(_FUNCT);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          }
-          return false;
-          break;
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_QWERTY);
+            }
+            return false;
+            break;
+        case FUNCT:  // Probably not needed?
+            if (record->event.pressed) {
+                layer_on(_FUNCT);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            } else {
+                layer_off(_FUNCT);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            }
+            return false;
+            break;
         case LOWER:
-          if (record->event.pressed) {
-            layer_on(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          } else {
-            layer_off(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          }
-          return false;
-          break;
+            if (record->event.pressed) {
+                layer_on(_LOWER);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            } else {
+                layer_off(_LOWER);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            }
+            return false;
+            break;
         case RAISE:
-          if (record->event.pressed) {
-            layer_on(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          } else {
-            layer_off(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          }
-          return false;
-          break;
-      }
+            if (record->event.pressed) {
+                layer_on(_RAISE);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            } else {
+                layer_off(_RAISE);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            }
+            return false;
+            break;
+    }
     return true;
 };
 
-bool muse_mode = false;
-uint8_t last_muse_note = 0;
-uint16_t muse_counter = 0;
-uint8_t muse_offset = 70;
-uint16_t muse_tempo = 50;
+bool     muse_mode      = false;
+uint8_t  last_muse_note = 0;
+uint16_t muse_counter   = 0;
+uint8_t  muse_offset    = 70;
+uint16_t muse_tempo     = 50;
 
 void encoder_update_user(uint8_t index, bool clockwise) {
-  if (muse_mode) {
-    if (IS_LAYER_ON(_RAISE)) {
-      if (clockwise) {
-        muse_offset++;
-      } else {
-        muse_offset--;
-      }
+    if (muse_mode) {
+        if (IS_LAYER_ON(_RAISE)) {
+            if (clockwise) {
+                muse_offset++;
+            } else {
+                muse_offset--;
+            }
+        } else {
+            if (clockwise) {
+                muse_tempo += 1;
+            } else {
+                muse_tempo -= 1;
+            }
+        }
     } else {
-      if (clockwise) {
-        muse_tempo+=1;
-      } else {
-        muse_tempo-=1;
-      }
+        if (clockwise) {
+            register_code(KC_PGDN);
+            unregister_code(KC_PGDN);
+        } else {
+            register_code(KC_PGUP);
+            unregister_code(KC_PGUP);
+        }
     }
-  } else {
-    if (clockwise) {
-      register_code(KC_PGDN);
-      unregister_code(KC_PGDN);
-    } else {
-      register_code(KC_PGUP);
-      unregister_code(KC_PGUP);
-    }
-  }
 }
 
 void dip_switch_update_user(uint8_t index, bool active) {
@@ -171,7 +129,6 @@ void dip_switch_update_user(uint8_t index, bool active) {
             }
     }
 }
-
 
 void matrix_scan_user(void) {
 #ifdef AUDIO_ENABLE
@@ -195,11 +152,11 @@ void matrix_scan_user(void) {
 }
 
 bool music_mask_user(uint16_t keycode) {
-  switch (keycode) {
-    case RAISE:
-    case LOWER:
-      return false;
-    default:
-      return true;
-  }
+    switch (keycode) {
+        case RAISE:
+        case LOWER:
+            return false;
+        default:
+            return true;
+    }
 }
